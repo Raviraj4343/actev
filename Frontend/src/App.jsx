7aa9d8dd0f8d2e6b3030d613a9e08a42bccfef89
vagabondraj <raviraj06112005@gmail.com>
2026-03-31 17:14:24 +0530

    add routes for DailyLog, Weight, Foods, and Insights pages in App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,6 +8,10 @@ import Landing from './pages/Landing'
 import SignIn from './pages/SignIn'
 import Signup from './pages/Signup'
 import DesignSystem from './pages/DesignSystem'
+import DailyLog from './pages/DailyLog'
+import Weight from './pages/Weight'
+import Foods from './pages/Foods'
+import Insights from './pages/Insights'
 
 export default function App(){
   return (
@@ -17,6 +21,10 @@ export default function App(){
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/daily" element={<DailyLog />} />
+        <Route path="/weight" element={<Weight />} />
+        <Route path="/foods" element={<Foods />} />
+        <Route path="/insights" element={<Insights />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/design" element={<DesignSystem />} />
