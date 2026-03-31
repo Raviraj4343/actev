4ad36a043607925bb8a9706708c0fe102a378adf
vagabondraj <raviraj06112005@gmail.com>
2026-03-31 17:37:09 +0530

    add sidebar toggle functionality to Layout component

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from './Sidebar'
 import Navbar from './Navbar'
 
 export default function Layout({ children }){
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
   return (
     <div className="app-root">
-      <Sidebar />
+      <Sidebar isOpen={sidebarOpen} onClose={()=>setSidebarOpen(false)} />
       <div className="main-area">
-        <Navbar />
-        <main className="content-area">
+        <Navbar onToggleSidebar={()=>setSidebarOpen(s=>!s)} />
+        <main className="content-area" onClick={()=> sidebarOpen && setSidebarOpen(false)}>
           {children}
         </main>
       </div>
